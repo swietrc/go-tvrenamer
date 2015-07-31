fff3809fa072a221d41db3c7c17ac2f87294db94
swietrich <simon.wietrich@gmail.com>
2015-07-31 15:51:38 +0200

    Add functions to initialize TvRenamer struct
    tvrenamer.New() now facilitated the init of TvRenamer struct.
    I also added functions to set values not set in New()

--- tvrenamer.go
+++ tvrenamer.go
@@ -1,5 +1,11 @@
 package tvrenamer
 
+import (
+	"log"
+	"regexp"
+	"text/template"
+)
+
 const (
 	// TVDB represents the tvdb API
 	TVDB uint8 = 0
@@ -7,13 +13,13 @@ const (
 	TVRAGE uint8 = 1
 )
 
-// TvRenamer stores the configuration used by the rename func
+// TvRenamer stores the configuration used by TvRenamer.Rename to rename files
 type TvRenamer struct {
 	CustomName     string
 	Language       string
-	NameFormatting string
+	NameFormatting *template.Template
 	NewPath        string
-	Regex          string
+	Regex          *regexp.Regexp
 	Scraper        uint8
 }
 
@@ -23,4 +29,34 @@ func (r *TvRenamer) Rename(filepath string) (err error) {
 		return
 	}
 	return
+}
+
+// SetSeriesName sets TvRenamer.CustomName to the given name
+func (r *TvRenamer) SetSeriesName(name string) {
+	r.CustomName = name
+}
+
+// SetScraper sets TvRenamer.Scraper to the given scraper
+func (r *TvRenamer) SetScraper(scraper uint8) {
+	r.Scraper = scraper
+}
+
+// New allocates a new TvRenamer with the given language, formatting, path and regexp
+func New(language string, nameFormatting string, newPath string, regex string) *TvRenamer {
+
+	var err error
+
+	tvr := TvRenamer{
+		Language: language,
+		NewPath:  newPath,
+	}
+
+	tvr.NameFormatting = template.New(nameFormatting)
+	tvr.Regex, err = regexp.Compile(regex)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &tvr
 }
