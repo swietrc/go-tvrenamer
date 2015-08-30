64be7f0f24a7155900065a7a08652cdac73e1ffd
swietrich <simon.wietrich@gmail.com>
2015-08-30 16:49:45 +0200

    Add function to get absolute path from relative path

--- tvrenamer.go
+++ tvrenamer.go
@@ -9,6 +9,7 @@ import (
 	"path"
 	"regexp"
 	"strconv"
+	"strings"
 	"text/template"
 )
 
@@ -32,27 +33,53 @@ type TvRenamer struct {
 	Language       string
 	Move           bool
 	NameFormatting *template.Template
-	NewPath        string
+	NewPath        *template.Template
 	Regex          *regexp.Regexp
 	Scraper        uint8
 }
 
 // Rename a file according to the config object passed as argument
 func (r *TvRenamer) Rename(filepath string) (err error) {
-	episode, err := r.getEpisode(filepath)
+	filepath, err = getAbsolutePath(filepath)
+	if err != nil {
+		log.Println(err)
+	}
+	ep, err := r.getEpisode(path.Base(filepath))
 	if err != nil {
 		return
 	}
 	var b bytes.Buffer
 	var newFilepath string
-	r.NameFormatting.Execute(&b, episode)
+	pathSNumber, _ := strconv.Atoi(ep.SeasonNb)
+	pathEpNumber, _ := strconv.Atoi(ep.EpisodeNb)
+	r.NameFormatting.Execute(&b, episode{
+		SeriesName:  ep.SeriesName,
+		EpisodeNb:   fmt.Sprintf("%02d", pathEpNumber),
+		SeasonNb:    fmt.Sprintf("%02d", pathSNumber),
+		EpisodeName: ep.EpisodeName,
+	})
 	newFilename := b.String() + path.Ext(filepath)
 	if r.Move {
-		newFilepath = path.Join(path.Base(filepath), newFilename)
+		b = bytes.Buffer{}
+
+		r.NewPath.Execute(&b, ep)
+		log.Println(b.String())
+		newFilepath = path.Join(b.String(), newFilename)
+		err = os.MkdirAll(path.Dir(newFilepath), 0777)
+		if err != nil {
+			log.Println("Unable to create dirs to path " + path.Dir(newFilepath))
+			log.Fatalln(err)
+		}
 	} else {
-		newFilepath = path.Join(r.NewPath, newFilename)
+		newFilepath = path.Join(path.Dir(filepath), newFilename)
 	}
-	os.Rename(filepath, newFilepath)
+	log.Println(newFilepath)
+	log.Println(newFilename)
+
+	err = os.Rename(filepath, newFilepath)
+	if err != nil {
+		log.Println(err)
+	}
 	return
 }
 
@@ -73,11 +100,15 @@ func New(language string, nameFormatting string, newPath string, regex string, m
 
 	tvr := TvRenamer{
 		Language: language,
-		NewPath:  newPath,
 		Move:     move,
 	}
 
 	tvr.NameFormatting, err = template.New("Filename formatting").Parse(nameFormatting)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tvr.NewPath, err = template.New("Path formatting").Parse(newPath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -95,13 +126,14 @@ func (r *TvRenamer) getEpisode(filepath string) (episode, error) {
 	match := r.Regex.FindStringSubmatch(path.Base(filepath))
 	episodeNb, _ := strconv.ParseUint(match[3], 10, 8)
 	seasonNb, _ := strconv.ParseUint(match[2], 10, 8)
+	log.Println(match)
 	switch r.Scraper {
 
 	case TVDB:
 		if r.CustomName != "" {
 			match[1] = r.CustomName
 		}
-		seriesList, err := tvdb.GetSeries(match[1], r.Language)
+		seriesList, err := tvdb.GetSeries(strings.Replace(match[1], ".", " ", -1), r.Language)
 		if len(seriesList.Series) == 0 {
 			return ep, err
 		}
@@ -109,11 +141,22 @@ func (r *TvRenamer) getEpisode(filepath string) (episode, error) {
 		tvdbSeries.GetDetails()
 		tvdbEp := seriesList.Series[0].Seasons[uint32(seasonNb)][uint32(episodeNb)-1]
 		ep = episode{
-			SeriesName:  tvdbSeries.SeriesName,
-			EpisodeNb:   fmt.Sprintf("%02d", tvdbEp.EpisodeNumber),
-			SeasonNb:    fmt.Sprintf("%02d", tvdbEp.SeasonNumber),
+			SeriesName: tvdbSeries.SeriesName,
+			// EpisodeNb:   fmt.Sprintf("%02d", tvdbEp.EpisodeNumber),
+			// SeasonNb:    fmt.Sprintf("%02d", tvdbEp.SeasonNumber),
+			EpisodeNb:   fmt.Sprint(tvdbEp.EpisodeNumber),
+			SeasonNb:    fmt.Sprint(tvdbEp.SeasonNumber),
 			EpisodeName: tvdbEp.EpisodeName,
 		}
 	}
 	return ep, nil
+}
+
+func getAbsolutePath(p string) (absPath string, err error) {
+	absPath = p
+	wd, err := os.Getwd()
+	if !path.IsAbs(p) {
+		absPath = path.Join(wd, p)
+	}
+	return
 }
